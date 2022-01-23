a1e93d415543bf1be933f044e8c39919c6016c0a
Genevieve Alfirevic <gen@nyble.dev>
2022-01-22 21:38:31 -0600

    We have a window 🥺

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,82 @@
+use glow::HasContext;
+use glutin::{
+    dpi::LogicalSize,
+    event::{Event, VirtualKeyCode, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    platform::{run_return::EventLoopExtRunReturn, unix::WindowBuilderExtUnix},
+    window::{Window, WindowBuilder},
+    ContextBuilder, ContextWrapper, PossiblyCurrent,
+};
+
 fn main() {
-    println!("Hello, world!");
+    let notsure = NotSure::run();
+}
+
+struct NotSure {
+    context: ContextWrapper<PossiblyCurrent, Window>,
+    gl: glow::Context,
+}
+
+impl NotSure {
+    pub fn run() -> ! {
+        let el = EventLoop::new();
+        let wb = WindowBuilder::new()
+            .with_title("notsure")
+            .with_app_id("pleasefloat".into())
+            .with_inner_size(LogicalSize::new(640.0, 480.0));
+        let wc = ContextBuilder::new()
+            .with_vsync(true)
+            .build_windowed(wb, &el)
+            .unwrap();
+        let context = unsafe { wc.make_current().unwrap() };
+
+        let gl = unsafe {
+            glow::Context::from_loader_function(|s| context.get_proc_address(s) as *const _)
+        };
+
+        unsafe {
+            gl.clear_color(0.4, 0.0, 0.6, 0.5);
+        }
+
+        let mut ns = Self { gl, context };
+
+        el.run(move |event, _, flow| ns.event_handler(event, flow))
+    }
+
+    pub fn draw(&self) {
+        unsafe {
+            self.gl.clear(glow::COLOR_BUFFER_BIT);
+        }
+    }
+
+    pub fn event_handler(&mut self, event: Event<()>, flow: &mut ControlFlow) {
+        match event {
+            Event::LoopDestroyed => return,
+            Event::WindowEvent { event, .. } => self.window_event(event, flow),
+            Event::RedrawRequested(_) => self.context.swap_buffers().unwrap(),
+            Event::MainEventsCleared => {
+                self.draw();
+                self.context.swap_buffers().unwrap();
+            }
+            _ => (),
+        }
+    }
+
+    pub fn window_event(&mut self, event: WindowEvent, flow: &mut ControlFlow) {
+        match event {
+            WindowEvent::Resized(physical) => {
+                self.context.resize(physical);
+                unsafe {
+                    self.gl
+                        .viewport(0, 0, physical.width as i32, physical.height as i32)
+                };
+            }
+            WindowEvent::KeyboardInput { input, .. } => {
+                if let Some(VirtualKeyCode::Escape) = input.virtual_keycode {
+                    *flow = ControlFlow::Exit;
+                }
+            }
+            _ => (),
+        }
+    }
 }
