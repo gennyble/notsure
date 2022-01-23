4033ac0a9c1b6c46b6402ea04f57621697644897
Genevieve Alfirevic <gen@nyble.dev>
2022-01-22 22:13:26 -0600

    Punt color into a module its own

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use std::str::FromStr;
+mod color;
+
+use color::Color;
 
 use confindent::Confindent;
 use glow::HasContext;
@@ -10,7 +12,6 @@ use glutin::{
     window::{Window, WindowBuilder},
     ContextBuilder, ContextWrapper, PossiblyCurrent,
 };
-use thiserror::Error;
 
 fn main() {
     let _notsure = NotSure::run();
@@ -111,56 +112,4 @@ impl Config {
 
         Self { clear_color }
     }
-}
-
-#[derive(Copy, Clone, Debug)]
-struct Color {
-    r: f32,
-    g: f32,
-    b: f32,
-    a: f32,
-}
-
-impl Color {
-    pub fn rgb(r: f32, g: f32, b: f32) -> Self {
-        Self { r, g, b, a: 1.0 }
-    }
-
-    pub fn rgba(r: f32, g: f32, b: f32, a: f32) -> Self {
-        Self { r, g, b, a }
-    }
-
-    pub fn grey(v: f32) -> Self {
-        Self {
-            r: v,
-            g: v,
-            b: v,
-            a: v,
-        }
-    }
-}
-
-impl FromStr for Color {
-    type Err = ColorParseError;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let numbers: Vec<f32> = s
-            .split(',')
-            .map(|s| s.trim().parse())
-            .collect::<Result<_, _>>()
-            .map_err(|_e| ColorParseError::InvalidColor(s.into()))?;
-
-        match numbers.len() {
-            1 => Ok(Self::grey(numbers[0])),
-            3 => Ok(Self::rgb(numbers[0], numbers[1], numbers[2])),
-            4 => Ok(Self::rgba(numbers[0], numbers[1], numbers[2], numbers[3])),
-            _ => Err(ColorParseError::InvalidColor(s.into())),
-        }
-    }
-}
-
-#[derive(Debug, Error)]
-enum ColorParseError {
-    #[error("The color {0} could not be parsed")]
-    InvalidColor(String),
 }
