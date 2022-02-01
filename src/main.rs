ca6b8e88815b1c93efdc1f76d341b01152f66ccd
Genevieve Alfirevic <gen@nyble.dev>
2022-01-31 20:12:34 -0600

    Grid and drawing grid and ahh
    The more you switch texture buffers, the faster you render

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ mod color;
 mod gl;
 mod vec2;
 
-use std::{cell::RefCell, cmp::Ordering, rc::Rc};
+use std::{cell::RefCell, cmp::Ordering, collections::HashMap, path::Path, rc::Rc, str::FromStr};
 
 use color::Color;
 use gl::{OpenGl, Texture};
@@ -51,6 +51,83 @@ impl Transform {
     }
 }
 
+#[derive(Copy, Clone, Debug, Hash, PartialEq, Eq)]
+enum Tile {
+    Nothing,
+    Spawn,
+    Air,
+    Ground,
+}
+
+struct Gridworld {
+    pub width: u32,
+    pub height: u32,
+    pub grid: Vec<Tile>,
+}
+
+impl Gridworld {
+    pub fn from_file<P: AsRef<Path>>(path: P) -> Self {
+        let contents = std::fs::read_to_string(path.as_ref()).unwrap();
+
+        contents.parse().unwrap()
+    }
+}
+
+impl FromStr for Gridworld {
+    type Err = &'static str;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut lines = s.lines();
+
+        let mut map: HashMap<char, Tile> = HashMap::new();
+        loop {
+            match lines.next().map(|s| s.trim()) {
+                None => return Err("Did not expect EOF"),
+                Some("") => break,
+                Some(s) => {
+                    let mut chars = s.chars();
+                    let indicator = chars.next().ok_or("No indicator in map line?")?;
+
+                    // Throwaway the pipe
+                    chars.next();
+
+                    let tile = match chars.collect::<String>().as_ref() {
+                        "Spawn" => Tile::Spawn,
+                        "Air" => Tile::Air,
+                        "Ground" => Tile::Ground,
+                        _ => panic!(),
+                    };
+
+                    map.insert(indicator, tile);
+                }
+            }
+        }
+
+        let mut width = 0;
+        let mut height = 0;
+        let mut grid = vec![];
+
+        for line in lines {
+            let chars = line.trim().chars();
+            width = line.trim().len() as u32;
+            height += 1;
+
+            for ch in chars {
+                match map.get(&ch) {
+                    Some(&tile) => grid.push(tile),
+                    None => todo!(),
+                }
+            }
+        }
+
+        Ok(Self {
+            width,
+            height,
+            grid,
+        })
+    }
+}
+
 fn main() {
     let command = std::env::args().nth(1);
 
@@ -68,11 +145,13 @@ struct NotSure {
     transform: Rc<RefCell<Transform>>,
 
     beescream: Texture,
+    gridworld: Gridworld,
+    tile_textures: HashMap<Tile, Texture>,
 }
 
 impl NotSure {
     pub fn run() -> ! {
-        let window_size = PhysicalSize::new(640, 240);
+        let window_size = PhysicalSize::new(640, 480);
 
         let el = EventLoop::new();
         let wb = WindowBuilder::new()
@@ -85,11 +164,21 @@ impl NotSure {
             .unwrap();
         let context = unsafe { wc.make_current().unwrap() };
 
-        let transform = Transform::new(window_size, 320);
+        let transform = Transform::new(window_size, 48);
         let wrapped_transform = Rc::new(RefCell::new(transform));
 
         let gl = OpenGl::new(&context, wrapped_transform.clone());
-        let beescream = Texture::from_file(&gl, "images/eightytest.png");
+        let beescream = Texture::from_file(&gl, "images/beescream.png");
+        let gridworld = Gridworld::from_file("test.grid");
+
+        let mut tile_textures = HashMap::new();
+        let tile_paths = vec![(Tile::Air, "air"), (Tile::Ground, "ground")];
+        for (tile, name) in tile_paths.into_iter() {
+            tile_textures.insert(
+                tile,
+                Texture::from_file(&gl, format!("images/tiles/{}.png", name)),
+            );
+        }
 
         println!("Setup OpenGL!");
 
@@ -100,6 +189,8 @@ impl NotSure {
             transform: wrapped_transform,
 
             beescream,
+            gridworld,
+            tile_textures,
         };
         ns.load_config();
 
@@ -120,10 +211,21 @@ impl NotSure {
         unsafe {
             self.gl.clear();
 
-            self.beescream.bind(&self.gl);
-            for x in 0..8 {
-                self.gl
-                    .draw_rectangle(((x as f32 - 4.0) * 0.25, 0.125).into(), (0.25, 0.25).into());
+            for (idx, tile) in self.gridworld.grid.iter().enumerate() {
+                if *tile == Tile::Spawn {
+                    continue;
+                }
+
+                let x =
+                    (idx as i32 % self.gridworld.width as i32) - (self.gridworld.width as i32 / 2);
+                let y =
+                    (idx as i32 / self.gridworld.width as i32) + (self.gridworld.height as i32 / 2);
+
+                self.tile_textures.get(tile).unwrap().bind(&self.gl);
+                self.gl.draw_rectangle(
+                    (x as f32, self.gridworld.height as f32 - y as f32).into(),
+                    (1.0, 1.0).into(),
+                )
             }
         }
     }
