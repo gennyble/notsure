9fc1fd581d24e998adf71c5def714cca5a584e18
Genevieve Alfirevic <gen@nyble.dev>
2022-05-19 15:31:34 -0500

    Add tests for aabb intersection

--- src/physics.rs
+++ src/physics.rs
@@ -28,3 +28,75 @@ where
 		false
 	}
 }
+
+#[cfg(test)]
+mod test {
+	use smitten::Vec2;
+
+	use crate::physics::aabb_check;
+
+	use super::AxisAlignedBoundingBox;
+
+	// A more traditional graphics model used here than the one in smitten
+	struct Thing {
+		center: Vec2,
+		half_size: Vec2,
+	}
+
+	impl AxisAlignedBoundingBox for Thing {
+		fn bottom_left(&self) -> Vec2 {
+			self.center - self.half_size
+		}
+
+		fn top_right(&self) -> Vec2 {
+			self.center + self.half_size
+		}
+	}
+
+	#[test]
+	fn intersection() {
+		let a = Thing {
+			center: Vec2::new(0.0, 0.0),
+			half_size: Vec2::new(2.0, 2.0),
+		};
+
+		let b = Thing {
+			center: Vec2::new(1.0, 1.0),
+			half_size: Vec2::new(2.0, 2.0),
+		};
+
+		assert!(aabb_check(&a, &b))
+	}
+
+	// What do I name this test? It makes sure that the intersection algorithm
+	// doesn't false positive if only X intersects
+	#[test]
+	fn x_axis_overlap_no_intersect() {
+		let a = Thing {
+			center: Vec2::new(0.0, 0.0),
+			half_size: Vec2::new(2.0, 2.0),
+		};
+
+		let b = Thing {
+			center: Vec2::new(1.0, 10.0),
+			half_size: Vec2::new(2.0, 2.0),
+		};
+
+		assert!(!aabb_check(&a, &b))
+	}
+
+	#[test]
+	fn y_axis_overlap_no_intersect() {
+		let a = Thing {
+			center: Vec2::new(0.0, 0.0),
+			half_size: Vec2::new(2.0, 2.0),
+		};
+
+		let b = Thing {
+			center: Vec2::new(10.0, 1.0),
+			half_size: Vec2::new(2.0, 2.0),
+		};
+
+		assert!(!aabb_check(&a, &b))
+	}
+}
