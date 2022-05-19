785afe14284da701721b7d09da0b8f52973aaac9
Genevieve Alfirevic <gen@nyble.dev>
2022-05-19 17:29:51 -0500

    Set point with Num 1 and 2 to draw line

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,10 @@ mod physics;
 
 use smitten::{self, Color, Draw, Smitten, Vec2, VirtualKeyCode};
 
-struct Thing {
+pub struct Thing {
 	center: Vec2,
+	previous_center: Vec2,
+
 	size: Vec2,
 	half_size: Vec2,
 	draw: Draw,
@@ -12,9 +14,12 @@ struct Thing {
 impl Thing {
 	pub fn new<C: Into<Vec2>, S: Into<Vec2>, D: Into<Draw>>(center: C, size: S, draw: D) -> Self {
 		let size = size.into();
+		let center = center.into();
 
 		Self {
-			center: center.into(),
+			center,
+			previous_center: center,
+
 			size,
 			half_size: size / 2,
 			draw: draw.into(),
@@ -30,6 +35,14 @@ impl physics::AxisAlignedBoundingBox for Thing {
 	fn top_right(&self) -> Vec2 {
 		self.center + self.half_size
 	}
+
+	fn previous_bottom_left(&self) -> Vec2 {
+		self.previous_center - self.half_size
+	}
+
+	fn previous_top_rght(&self) -> Vec2 {
+		self.previous_center + self.half_size
+	}
 }
 
 fn main() {
@@ -39,6 +52,9 @@ fn main() {
 	let mut us = Thing::new((-3.0, -3.0), (1, 1), Color::rgb(0.1, 0.3, 0.5));
 	let mut intersecting = false;
 	let speed = 0.075;
+
+	let mut point_a = None;
+	let mut point_b = None;
 
 	loop {
 		let _events = smitty.events();
@@ -59,6 +75,14 @@ fn main() {
 			us.center.x += speed;
 		}
 
+		if smitty.is_key_down(VirtualKeyCode::Key1) {
+			point_a = Some(us.center);
+		}
+
+		if smitty.is_key_down(VirtualKeyCode::Key2) {
+			point_b = Some(us.center);
+		}
+
 		match physics::aabb_check(&us, &square) {
 			true if !intersecting => {
 				us.draw = Color::rgb(0.5, 0.1, 0.2).into();
@@ -74,6 +98,15 @@ fn main() {
 		smitty.clear();
 		smitty.rect(square.center, square.size, square.draw);
 		smitty.rect(us.center, us.size, us.draw);
+
+		if let Some(p) = point_a {
+			smitty.rect(p, (0.25, 0.25), Color::rgb(0.6, 0.6, 0.2))
+		}
+
+		if let Some(p) = point_b {
+			smitty.rect(p, (0.25, 0.25), Color::rgb(0.2, 0.6, 0.2))
+		}
+
 		smitty.swap();
 	}
 }
