cf8ed4f88b5d17a63c2b7d3b5b63febf32c5b705
Genevieve Alfirevic <gen@nyble.dev>
2022-05-27 20:52:45 -0500

    Some grid work

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,15 @@ use thing::Thing;
 
 fn main() {
 	let mut smitty = Smitten::new((720, 480), "Square", 36);
+	smitty.texture_coloring(false);
 
-	let square = Thing::new((0, 0), (1, 1), smitty.make_texture("images/puare.png"));
+	let sq = smitty.make_texture("images/puare.png");
+
 	let mut us = Thing::new((-3.0, -3.0), (1, 1), Color::rgb(0.1, 0.3, 0.5));
 	let speed = 0.075;
+
+	let mut grid = Grid::new((5, 5), (1, 1), 1.0);
+	let gridlines = grid.gridlines();
 
 	loop {
 		let _events = smitty.events();
@@ -32,10 +37,34 @@ fn main() {
 			us.offset((speed, 0.0));
 		}
 
-		// Drawing
+		if smitty.is_key_down(VirtualKeyCode::E) {
+			if let Some(coords) = grid.get_coords(us.center) {
+				grid.set_tile(coords, Some(Tile::Solid));
+			}
+		}
 
+		// Drawing
 		smitty.clear();
-		smitty.rect(square.center, square.size, square.draw);
+
+		for line in &gridlines {
+			smitty.sdf(*line);
+		}
+
+		for (tl, p) in grid.tiles_and_position() {
+			match tl {
+				Some(Tile::Solid) => smitty.rect(p, (grid.side_length, grid.side_length), sq),
+				_ => (),
+			}
+		}
+
+		if let Some(p) = grid.coordinate_center(2, 2) {
+			smitty.sdf(SignedDistance::Circle {
+				center: p,
+				radius: 2,
+				color: Color::YELLOW,
+			})
+		}
+
 		smitty.rect(us.center, us.size, us.draw);
 
 		smitty.swap();
@@ -96,4 +125,159 @@ fn do_full_collision(dynamic: &mut Thing, stuck: &Thing, movement: Vec2) -> bool
 		return true;
 	}
 	false
+}
+
+#[derive(Copy, Clone, Debug)]
+struct Size {
+	width: u32,
+	height: u32,
+}
+
+impl Size {
+	pub fn stride(&self) -> usize {
+		self.width as usize * self.height as usize
+	}
+
+	pub fn half(&self) -> Vec2 {
+		Vec2::new(self.width as f32 / 2.0, self.height as f32 / 2.0)
+	}
+}
+
+impl From<(u32, u32)> for Size {
+	fn from(t: (u32, u32)) -> Self {
+		Size {
+			width: t.0,
+			height: t.1,
+		}
+	}
+}
+
+impl From<Size> for Vec2 {
+	fn from(s: Size) -> Self {
+		Vec2 {
+			x: s.width as f32,
+			y: s.width as f32,
+		}
+	}
+}
+
+struct Grid {
+	size: Size,
+	position: Vec2,
+
+	side_length: f32,
+	grid: Vec<Option<Tile>>,
+}
+
+impl Grid {
+	pub fn new<S: Into<Size>, P: Into<Vec2>>(size: S, position: P, side_length: f32) -> Self {
+		let size = size.into();
+
+		Self {
+			size,
+			position: position.into(),
+			side_length,
+			grid: vec![None; size.stride()],
+		}
+	}
+
+	pub fn gridlines(&self) -> Vec<SignedDistance> {
+		let mut lines = vec![];
+
+		let half_size = self.size.half();
+
+		let x_start = self.position.x - half_size.x;
+		let x_end = self.position.x + half_size.x;
+
+		let y_start = self.position.y - half_size.y;
+		let y_end = self.position.y + half_size.y;
+
+		for x in 0..=self.size.width {
+			let x_position = (x_start + x as f32) * self.side_length;
+
+			lines.push(SignedDistance::LineSegment {
+				start: Vec2::new(x_position, y_start),
+				end: Vec2::new(x_position, y_end),
+				thickness: 2,
+				color: Color::rgb(0.5, 0.3, 0.0),
+			});
+		}
+
+		for y in 0..=self.size.height {
+			let y_position = (y_start + y as f32) * self.side_length;
+
+			lines.push(SignedDistance::LineSegment {
+				start: Vec2::new(x_start, y_position),
+				end: Vec2::new(x_end, y_position),
+				thickness: 2,
+				color: Color::rgb(0.5, 0.3, 0.0),
+			});
+		}
+
+		lines
+	}
+
+	pub fn coordinate_center(&self, x: u32, y: u32) -> Option<Vec2> {
+		if x >= self.size.width && y >= self.size.height {
+			return None;
+		}
+
+		let half_size = self.size.half();
+		Some(Vec2 {
+			x: (x as f32 - half_size.x) * self.side_length
+				+ (self.side_length / 2.0)
+				+ self.position.x,
+			y: (y as f32 - half_size.y) * self.side_length
+				+ (self.side_length / 2.0)
+				+ self.position.y,
+		})
+	}
+
+	//TODO: gen- function name?
+	pub fn get_coords(&self, loc: Vec2) -> Option<Size> {
+		let offset_to_center = loc - self.position;
+
+		//TODO: gen- Why uh, why is it half_size PLUS offset?
+		let half_size = self.size.half();
+		let coords = (half_size + offset_to_center) / self.side_length;
+
+		if coords.x < 0.0
+			|| coords.y < 0.0
+			|| coords.x > self.size.width as f32
+			|| coords.y > self.size.height as f32
+		{
+			None
+		} else {
+			Some(Size {
+				width: coords.x as u32,
+				height: coords.y as u32,
+			})
+		}
+	}
+
+	pub fn set_tile(&mut self, coords: Size, tile: Option<Tile>) {
+		//TODO: gen- check coordinates valid
+		self.grid[coords.height as usize * self.size.width as usize + coords.width as usize] = tile;
+	}
+
+	pub fn tiles_and_position(&self) -> Vec<(Option<&Tile>, Vec2)> {
+		let mut ret = vec![];
+
+		for (idx, tile) in self.grid.iter().enumerate() {
+			let y = idx / self.size.width as usize;
+			let x = idx % self.size.width as usize;
+
+			ret.push((
+				tile.as_ref(),
+				self.coordinate_center(x as u32, y as u32).unwrap(),
+			))
+		}
+
+		ret
+	}
+}
+
+#[derive(Clone, Copy, Debug)]
+enum Tile {
+	Solid,
 }
